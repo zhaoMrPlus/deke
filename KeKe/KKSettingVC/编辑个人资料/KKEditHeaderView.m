da9af8617afe278cd692c90c4ff9bb1be9ee719e
zhaohongbin1111 <15671056747@163.com>
2018-02-07 15:45:00 +0800

    bug fix

--- KeKe/KKSettingVC/编辑个人资料/KKEditHeaderView.m
+++ KeKe/KKSettingVC/编辑个人资料/KKEditHeaderView.m
@@ -91,7 +91,7 @@
 /*
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect 上半身
+- (void)drawRect:(CGRect)rect
     // Drawing code
 }
 */
